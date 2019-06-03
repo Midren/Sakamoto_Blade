da0f35dec93bd27baa1ea57868adeef92ecb6e66
Roman Milishchuk <milromchuk@gmail.com>
2019-06-04 02:30:36 +0300

    Added async loading images needed for game

--- src/main-client.js
+++ src/main-client.js
@@ -3,9 +3,48 @@ const socket = io();
 const canvas = document.getElementById("field");
 const ctx = canvas.getContext("2d");
 
-const startGame = () => {
+const loadImg = (path, imagesSrc) => new Promise((res, rej) => {
+    let images = {};
+
+    let loadedImages = 0;
+    let imagesNumber = imagesSrc.length;
+    imagesSrc.forEach((val) => {
+        let img = new Image();
+        img.addEventListener('load', () => {
+            images[val] = img;
+            if (++loadedImages === imagesNumber)
+                res(images);
+        });
+        img.src = path + val;
+    });
+});
+
+let playerImagesSrc = ["0l.png", "atkL2.png", "atkR3.png", "l1.png", "l5.png", "r2.png", "r6.png", "0.png",
+    "atkL3.png", "jl.png", "l2.png", "l6.png", "r3.png", "0r.png", "atkR1.png", "j.png", "l3.png",
+    "Pistol.png", "r4.png", "atkL1.png", "atkR2.png", "jr.png", "l4.png", "r1.png", "r5.png"];
+
+let blocksImagesSrc = ["lava.png", "platform.png", "water.png"];
+
+let backgroundImagesSrc = Array(171).fill(0).map((val, i) =>
+    "frame_" + i.toString().padStart(3, "0") + ".png");
+
+let playerImg = loadImg("img/player/", playerImagesSrc);
+let blocksImg = loadImg("img/blocks/", blocksImagesSrc);
+let backgroundImg = loadImg("img/background/", backgroundImagesSrc);
+
+const startGame = (playerImg, blocksImg, backgroundImg) => {
+    document.getElementById("loading_screen").style.display = "none";
+    document.getElementById("field").style.display = "flex";
+    ctx.drawImage(backgroundImg["frame_000.png"], 0, 0);
+    ctx.drawImage(playerImg["0l.png"], 550, 240, 100, 80);
+    ctx.drawImage(blocksImg["platform.png"], 550, 320, 100, 100);
     socket.emit("game-start");
 };
 
-socket.on("connect", startGame);
+const loadingGame = () => playerImg.then((player) =>
+    blocksImg.then((blocks) =>
+        backgroundImg.then((background) =>
+            startGame(player, blocks, background))));
+
+socket.on("connect", loadingGame);
 
